44f27b5882d64709eb001c3e3ae9661e7fbaadff
MiloShen <Milo-Shen@outlook.com>
2022-01-29 16:08:19 +0800

    feat: 新增字符串字面值

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,17 @@ fn mut_calculate_length(x: &mut String) -> usize {
     x.len()
 }
 
+fn first_world(x: &str) -> &str {
+    // todo: &[u8] 是什么 ?
+    let _bytes = x.as_bytes();
+    for (i, &item) in _bytes.iter().enumerate() {
+        if item == b' ' {
+            return &x[..i];
+        }
+    }
+    return &x[..];
+}
+
 fn main() {
     // Variables and Mutability
     println!("rust course version is: {}", VERSION);
@@ -164,4 +175,23 @@ fn main() {
     let world: &str = &str_slice[6..];
     let whole: &str = &str_slice[..];
     println!("{},{},{}", hello, world, whole);
+
+    // array slice
+    let origin_array = [1; 4];
+    let array_slice = &origin_array[..2];
+    println!("array_slice index 1 is: {}", array_slice[1]);
+
+    // mut array slice
+    let mut origin_array = [1; 4];
+    let array_slice = &mut origin_array[..2];
+    array_slice[0] = 10;
+    // todo: 切片本身是否是一种引用 ? 改变引用会不会改变原先的值 ? ( mut 的类型 )
+    // todo: 下面 2 句话: 173 和 174 对调就会产生 error，详细了解为什么
+    println!("slice index 0 value: {}", array_slice[0]);
+    println!("origin index 0 value: {}", origin_array[0]);
+
+    // todo: 字符串字面值的类型就是字符串切片 ! 所以不需要再 &
+    let whole = "Hello World";
+    let find = first_world(whole);
+    println!("first world is: {}", find);
 }
