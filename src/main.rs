eb9db6d08f68d3349c87d8f8f07d6447d3612777
MiloShen <Milo-Shen@outlook.com>
2022-01-30 11:27:15 +0800

    feat: add struct example

--- src/main.rs
+++ src/main.rs
@@ -42,6 +42,12 @@ fn first_world(x: &str) -> &str {
     return &x[..];
 }
 
+fn empty_function() {}
+
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
+}
+
 fn main() {
     // Variables and Mutability
     println!("rust course version is: {}", VERSION);
@@ -82,6 +88,10 @@ fn main() {
     };
 
     println!("The value of y is: {}", y);
+
+    // Empty Functions
+    // todo: The return value of empty function is ();
+    let empty_result = empty_function();
 
 
     // Control Flow
@@ -219,5 +229,56 @@ fn main() {
     struct Point(f32, f32, f64);
     let black: Color = Color(0, 0, 0);
     let point: Point = Point(1.0, 1.0, 1.0);
-    println!("{}, {}", black.0, point.0)
+    println!("{}, {}", black.0, point.0);
+
+    // Unit-Like Struct
+    struct UnitLikeStruct {}
+
+    // Struct Reference - LifeCycle
+    // Missing lifetime specifier [E0106]
+    // struct LifeCycle {
+    //     name: &str,
+    // }
+
+
+    // The exercise of struct
+
+    // 让 Rectangle 派生于 Debug
+    #[derive(Debug)]
+    struct Rectangle {
+        width: u32,
+        height: u32,
+    }
+
+    fn area(rect: &Rectangle) -> u32 {
+        return rect.width * rect.height;
+    }
+
+    let rect = Rectangle {
+        width: 30,
+        height: 40,
+    };
+
+    println!("The area of rect is: {}", area(&rect));
+    println!("The rect is: {:#?}", rect);
+
+
+    // Method of struct
+    impl Rectangle {
+        fn area(&self) -> u32 {
+            return self.width * self.height;
+        }
+
+        // 关联函数
+        fn print(x: &str) {
+            println!("static function print: {}", x);
+        }
+    }
+
+    let area = rect.area();
+    println!("The area is: {}", area);
+    Rectangle::print("My Print");
+
+
+    // 枚举与模式匹配
 }
