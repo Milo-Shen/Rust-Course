3cf550bee29e53f4677723e6f2131c7142740a63
Milo-Shen <Milo-Shen@outlook.com>
2022-02-04 12:19:02 +0800

    feat: refactor struct module

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,6 @@ mod front_of_house;
 // 引入包内所有的内容
 // use std::collections::*;
 
-fn print_type_of<T>(_: &T) {
-    println!("{}", std::any::type_name::<T>())
-}
-
 // mod tree
 mod variables;
 mod functions;
@@ -21,6 +17,7 @@ mod control_flow;
 mod ownership;
 mod reference;
 mod slices;
+mod structs;
 mod hashmap;
 
 fn main() {
@@ -42,80 +39,8 @@ fn main() {
     // Slices
     slices::learning_slice();
 
-    // struct
-    struct User {
-        name: String,
-        age: u8,
-    }
-    let mut jack: User = User {
-        age: 1,
-        name: String::from("jack"),
-    };
-    let marry = User {
-        name: String::from("Mary"),
-        ..jack
-    };
-    println!("name: {}, {}", jack.name, marry.name);
-
-    // update the value of struct User for jack variable
-    jack.name = String::from("Jack Upgrade");
-    println!("name: {}, {}", jack.name, marry.name);
-
-    // tuple struct
-    struct Color(i32, i32, i32);
-    struct Point(f32, f32, f64);
-    let black: Color = Color(0, 0, 0);
-    let point: Point = Point(1.0, 1.0, 1.0);
-    println!("{}, {}", black.0, point.0);
-
-    // Unit-Like Struct
-    struct UnitLikeStruct {}
-
-    // Struct Reference - LifeCycle
-    // Missing lifetime specifier [E0106]
-    // struct LifeCycle {
-    //     name: &str,
-    // }
-
-
-    // The exercise of struct
-
-    // 让 Rectangle 派生于 Debug
-    #[derive(Debug)]
-    struct Rectangle {
-        width: u32,
-        height: u32,
-    }
-
-    fn area(rect: &Rectangle) -> u32 {
-        return rect.width * rect.height;
-    }
-
-    let rect = Rectangle {
-        width: 30,
-        height: 40,
-    };
-
-    println!("The area of rect is: {}", area(&rect));
-    println!("The rect is: {:#?}", rect);
-
-
-    // Method of struct
-    impl Rectangle {
-        fn area(&self) -> u32 {
-            return self.width * self.height;
-        }
-
-        // 关联函数
-        fn print(x: &str) {
-            println!("static function print: {}", x);
-        }
-    }
-
-    let area = rect.area();
-    println!("The area is: {}", area);
-    Rectangle::print("My Print");
-
+    // Struct
+    structs::learning_struct();
 
     // 枚举与模式匹配
     enum IpAddKind {
