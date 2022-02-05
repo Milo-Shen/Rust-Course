1c9d45dc0a0fb8a6a357c239f532bc90f5a16f45
MiloShen <Milo-Shen@outlook.com>
2022-02-05 19:26:46 +0800

    feat: add examples to panic

--- src/panic.rs
+++ src/panic.rs
@@ -10,4 +10,26 @@ pub fn learning_panic() {
     // note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
     // todo: mac os 上使用 set RUST_BACKTRACE=1 无效
     // let b = arr[100];
+
+    struct Guess {
+        _value: i32,
+    }
+
+    impl Guess {
+        fn new(value: i32) -> Guess {
+            if value < 1 || value > 100 {
+                panic!("Guess value must be between 1 and 100, got {}", value);
+            }
+            return Guess { _value: value };
+        }
+
+        fn value(&self) -> i32 {
+            return self._value;
+        }
+    }
+
+    let mut guess_num = Guess::new(50);
+    guess_num._value = 100;
+    let my_num = guess_num.value();
+    println!("The guess value is: {}", my_num);
 }
