9f94d25d7e68042c114a6d0be833d03a703b0f0d
MiloShen <Milo-Shen@outlook.com>
2022-02-11 21:42:58 +0800

    feat: improved error handling

--- src/example_grep.rs
+++ src/example_grep.rs
@@ -1,5 +1,5 @@
 use std::env::args;
-use std::fs;
+use std::{fs, process};
 
 pub fn learning_grep_example() {
     println!("Start to learn grep example");
@@ -13,17 +13,21 @@ struct Config {
 impl Config {
     // 此处是返回 Config 所以可以把原先的 parse_config 函数改编成如下的 new 关联函数的形式
     // args 是一个 Vec 的切片
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        // 异常处理
+        if args.len() < 3 {
+            return Err("not enough arguments !");
+        }
         // todo: 如果使用 arg[1] 而不是 &arg[1], 会报错: error[E0507]: cannot move out of index of `Vec<String>`
         // todo: 思考为什么，顺带不用 clone 的话，会提示 can not move
         let query = args[1].clone();
         let mut filename = String::from("./assets/");
         filename.push_str(&args[2]);
         let _filename = filename;
-        return Config {
+        return Ok(Config {
             query,
             filename: _filename,
-        };
+        });
     }
 }
 
@@ -42,7 +46,10 @@ pub fn my_grep() {
     let args: Vec<String> = args().collect();
     // 若是我们运行: cargo run a b 会输出: ["target/debug/rust_course", "a", "b"]
     println!("{:?}", args);
-    let config: Config = Config::new(&args);
+    let config: Config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
     println!("Search for {}, in file: {}", config.query, config.filename);
     let contents = fs::read_to_string(config.filename).expect("Something went wrong when reading the file");
     //  cargo run Nobody poem.txt
