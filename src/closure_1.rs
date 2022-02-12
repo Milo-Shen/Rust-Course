8f337e12884e51174d42fe987f1560fdb4620a8a
MiloShen <Milo-Shen@outlook.com>
2022-02-12 14:57:26 +0800

    feat: finish closure 2

--- src/closure_1.rs
+++ src/closure_1.rs
@@ -10,6 +10,17 @@ pub fn learning_closure() {
     //  - 保存为变量、作为参数
     //  - 可以在一个地方创建闭包，然后在另一个上下文中调用闭包来完成运算
     //  - 可以从其定义的作用域捕获值
+
+    // 闭包的类型推断
+    //  - 注意: 闭包的定义最终只会为参数 / 返回值推断出唯一具体的类型
+    // todo: 为什么加个 return 就不提示: error[E0282]: type annotations needed
+    // 不标注类型，且没有被调用时，无法通过编译，会提示添加类型申明
+    let example_closure = |x| x;
+    // 调用下方函数后，x 的类型被确定为了 &str
+    let s = example_closure("Hello Closure");
+    // 因为上面语句的执行中 x 已经是 &str 了，所以再赋 int 类型是错误的，
+    // 错误提示为: error[E0308]: mismatched types, expected `&str`, found integer
+    let n = example_closure(5);
 }
 
 fn simulated_expensive_calculation(intensity: u32) -> u32 {
@@ -49,3 +60,6 @@ fn generate_workout(intensity: u32, random_number: u32) {
 // fn add_one_v2 = | x: u32 | -> u32 { x + 1 };
 // fn add_one_v3   |x|               { x + 1 };
 // fn add_one_v4   |x|                 x + 1  ;
+
+// 闭包的类型推断
+//  - 注意: 闭包的定义最终只会为参数 / 返回值推断出唯一具体的类型
