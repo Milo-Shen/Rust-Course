cc82464a38f0df40d0e233a43442b49a94660107
MiloShen <Milo-Shen@outlook.com>
2022-02-13 21:26:40 +0800

    feat: add Cache example

--- src/closure_1.rs
+++ src/closure_1.rs
@@ -20,25 +20,69 @@ pub fn learning_closure() {
     let s = example_closure("Hello Closure");
     // 因为上面语句的执行中 x 已经是 &str 了，所以再赋 int 类型是错误的，
     // 错误提示为: error[E0308]: mismatched types, expected `&str`, found integer
-    let n = example_closure(5);
+    // let n = example_closure(5);
+}
+
+struct Cache<T>
+    where T: Fn(u32) -> u32 {
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Cache<T>
+    where T: Fn(u32) -> u32 {
+    fn new(calculation: T) -> Cache<T> {
+        return Cache {
+            calculation,
+            value: None,
+        };
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                return v;
+            }
+        }
+    }
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
     // 此处是把匿名函数的定义传给了变量 expensive_closure
     // 此时 expensive_closure 的函数签名为: fn(u32) -> u32
-    let expensive_closure: fn(u32) -> u32 = |num: u32| -> u32 {
+    let mut expensive_closure: Cache<fn(u32) -> u32> = Cache::new(|num: u32| -> u32 {
         println!("calculating slowly... ");
         thread::sleep(Duration::from_secs(1));
         return num;
-    };
+    });
     if intensity < 25 {
-        println!("Today, do {} push ups!", expensive_closure(intensity));
-        println!("Next, do {} sit ups", expensive_closure(intensity));
+        // 此处的 expensive_closure 会被执行 2 次，比较浪费性能，下面给出优化的手段
+        // 创建一个 struct，它持有闭包及其调用的结果:
+        //  - 只会在需要结果时才执行该闭包
+        //  - 可缓存结果
+        // 这个模式通常叫做记忆化 ( memoization ) 或延迟计算 ( lazy evaluation )
+
+        // 如何让 struct 持有闭包
+        // struct 的定义需要知道所有字段的类型
+        //  - 需要知名闭包的类型
+        // 每个闭包实例都有自己唯一的匿名类型,即使两个闭包签名完全一致
+        // 所以需要使用: 泛型和 Trait Bound
+
+        // Fn Trait ( 由标准库提供 )
+        // 所有的闭包都实现了至少以下 Trait 之一 :
+        //  - Fn
+        //  - FnMut
+        //  - FnOnce
+        println!("Today, do {} push ups!", expensive_closure.value(intensity));
+        println!("Next, do {} sit ups", expensive_closure.value(intensity));
     } else {
         if random_number == 3 {
             println!("Take a break today! Remember to stay hydrated!");
         } else {
-            println!("Today, run for {} minutes!", expensive_closure(intensity));
+            println!("Today, run for {} minutes!", expensive_closure.value(intensity));
         }
     }
 }
