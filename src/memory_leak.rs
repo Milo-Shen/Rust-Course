37e06b3e9710236d286d20ea7706379be6d287de
MiloShen <Milo-Shen@outlook.com>
2022-02-27 15:03:17 +0800

    feat: enhance weak example

--- src/memory_leak.rs
+++ src/memory_leak.rs
@@ -1,5 +1,5 @@
-use std::rc::Rc;
-use std::cell::RefCell;
+use std::rc::{Rc, Weak};
+use std::cell::{Ref, RefCell};
 
 // 如此我们可以灵活修改 Cons 来让他指向下一个 list 值
 #[derive(Debug)]
@@ -58,7 +58,7 @@ pub fn learning_memory_leak() {
     // Rc<T> 实例通过调用 Rc::downgrade 方法可以创建 Weak Reference ( 弱引用 )
     //  - 返回类型是 Weak<T> ( 智能指针 )
     //  - 调用 Rc::downgrade 会为 weak_count + 1
-    // Rc<T> 使用 weak_count 开追踪存在多少 Weak<T>
+    // Rc<T> 使用 weak_count 来追踪存在多少 Weak<T>
     // weak_count 不为 0 并不影响 Rc<T> 实例的清理
 
     // Strong vs Weak
@@ -68,4 +68,29 @@ pub fn learning_memory_leak() {
     //  - 当 Strong Reference 数量为 0 的时候, Weak Reference 会自动断开
     // 在使用 Weak<T> 前, 需保证它指向的值仍然存在:
     //  - 在 Weak<T> 实例上调用 upgrade 方法, 返回 Option<Rc<T>>
+    #[derive(Debug)]
+    struct Node {
+        value: i32,
+        parent: RefCell<Weak<Node>>,
+        children: RefCell<Vec<Rc<Node>>>,
+    }
+
+    // 目前 leaf 里的 node 有两个所有者, 一个是 leaf, 另一个是 branch 的 children
+    let leaf = Rc::new(Node {
+        value: 3,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![]),
+    });
+
+    println!("leaf parent  = {:?}", leaf.parent.borrow().upgrade());
+
+    let branch = Rc::new(Node {
+        value: 5,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![Rc::clone(&leaf)]),
+    });
+
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+    // 因为使用了 Weak, 所以下面这段代码没有造成循环引用
+    println!("leaf parent  = {:#?}", leaf.parent.borrow().upgrade());
 }
