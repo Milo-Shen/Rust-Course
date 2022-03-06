71f875317750b32d8c0bad68aeaf2fcb1d2bf218
MiloShen <Milo-Shen@outlook.com>
2022-03-06 20:11:50 +0800

    feat: add destructure enum example

--- src/pattern_matching_3.rs
+++ src/pattern_matching_3.rs
@@ -20,4 +20,72 @@ pub fn learning_pattern_matching() {
         _ => println!("Default case, x = {:?}", x)
     }
     println!("At the end: x= {:?},y = {}", x, y);
+
+    // 多重模式
+    // 在 match 表达式中, 使用 | 语法 ( 就是或的意思 ), 可以匹配多种模式
+    let x = 1;
+    match x {
+        1 | 2 => println!("one"),
+        3 => println!("three"),
+        _ => println!("other"),
+    }
+
+    // 使用 ..= 来匹配某个范围的值
+    let x = 5;
+    match x {
+        1..=5 => println!("1 ~ 5"),
+        _ => println!("something else"),
+    }
+
+    let x = 'c';
+    match x {
+        'a'..='j' => println!("early ASCII letter"),
+        'k'..='z' => println!("late ASCII letter"),
+        _ => println!("something else"),
+    }
+
+    // 结构以分解值
+    // 可以使用模式来解构 struct、enum、tuple, 从而引用这些类型值的不同部分
+    struct Point {
+        x: i32,
+        y: i32,
+    }
+
+    let p = Point { x: 0, y: 7 };
+    let Point { x: a, y: b } = p;
+    // 输出: a = 0, b = 7
+    println!("a = {}, b = {}", a, b);
+    let Point { x, y } = p;
+    println!("x = {}, y = {}", x, y);
+
+    match p {
+        Point { x, y: 0 } => println!("On the x axis at {}", x),
+        Point { x: 0, y } => println!("On the y axis at {}", y),
+        Point { x, y } => println!("On neither axis ({},{})", x, y),
+    }
+
+    // 解构枚举 enum
+    #[derive(Debug)]
+    enum Message {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(i32, i32, i32),
+    }
+
+    let msg = Message::ChangeColor(0, 160, 255);
+    match msg {
+        Message::Quit => {
+            println!("The quit variant has no data to destructure");
+        }
+        Message::Move { x, y } => {
+            println!("Move in the x direction {} and in the y direction {}", x, y);
+        }
+        Message::Write(text) => {
+            println!("Text Message {}", text);
+        }
+        Message::ChangeColor(r, g, b) => {
+            println!("change the color: r = {} ,g = {} ,b = {}", r, g, b);
+        }
+    }
 }
