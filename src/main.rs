d058d387e194d65687d9cd07ff273d21d6325e55
Milo-Shen <Milo-Shen@outlook.com>
2023-03-19 16:19:54 +0800

    feat: update comments in package system

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use std::io::Result as IoResult;
 // use 的简写形式
 use std::io::{self, Write};
 
-// todo: 需要研究下 项目名:: 这种用法
 use rust_course::eat_at_restaurant;
 
 mod front_of_house;
@@ -95,115 +94,112 @@ fn main() {
 
     // match 关键字
     // 允许一个值与一系列模式进行匹配, 模式
-    match_syntax::learning_match();
-    return;
-    ;
+    // match_syntax::learning_match();
 
     // 使用 module 下的函数
     eat_at_restaurant();
     front_of_house::hosting::print_house();
-    front_of_house::hosting::print_house();
 
-    // vector
-    vector::learning_vector();
-
-    // string
-    string::learning_string();
-
-    // hashmap 章节
-    hashmap::learning_hashmap();
-
-    // panic
-    panic::learning_panic();
-
-    // result
-    result::learning_result();
-
-    // eliminate duplicated code
-    eliminate_duplicate_code::learning_eliminate();
-
-    // generics
-    generics::learning_generics();
-
-    // trait
-    learning_trait::learning_trait();
-
-    // lifecycle
-    lifecycle_1::learning_lifecycle();
-    lifecycle_2::learning_lifecycle();
-    lifecycle_3::learning_lifecycle();
-    lifecycle_4::learning_lifecycle();
-
-    // test
-    learning_test::learning_tests();
-
-    // grep example
-    example_grep::learning_grep_example();
-    example_grep::my_grep();
-
-    // closure
-    closure_1::learning_closure();
-    closure_2::learning_closure();
-
-    // iterator
-    iterator_1::learning_iterator();
-    iterator_2::learning_iterator();
-    iterator_3::learning_iterator();
-    iterator_4::learning_iterator();
-    iterator_5::learning_iterator();
-    iterator_6::learning_iterator();
-
-    // pub use
-    learning_pub_use::learning_pub_use();
-
-    // smart pointer
-    smart_pointer_begin::learning_smart_pointer();
-    smart_pointer_box::learning_smart_pointer();
-
-    // deref trait
-    deref_trait_1::learning_deref_trait();
-    deref_trait_2::learning_deref_trait();
-
-    // drop trait
-    drop_trait::learning_drop_trait();
-
-    // Rc<T> 引用计数智能指针
-    rc::learning_rc();
-
-    // RecCell<T>
-    refcell::learning_ref_cell();
-
-    // memory leak
-    memory_leak::learning_memory_leak();
-
-    // multi threads
-    threads_1::learning_threads();
-    threads_2::learning_threads();
-    threads_3::learning_threads();
-    threads_4::learning_threads();
-
-    // oop
-    oop_1::learning_oop();
-    oop_2::learning_oop();
-    oop_3::learning_oop();
-
-    // pattern matching
-    pattern_matching_1::learning_pattern_matching();
-    pattern_matching_2::learning_pattern_matching();
-    pattern_matching_3::learning_pattern_matching();
-
-    // unsafe rust
-    unsafe_rust::learning_unsafe_rust();
-
-    // advanced trait
-    advanced_trait::learning_advanced_trait();
-
-    // advanced type
-    advanced_type::learning_advanced_type();
-
-    // advanced function
-    advanced_function::learning_advanced_function();
-
-    // marcos
-    learn_macro::learning_macros();
+    // // vector
+    // vector::learning_vector();
+    //
+    // // string
+    // string::learning_string();
+    //
+    // // hashmap 章节
+    // hashmap::learning_hashmap();
+    //
+    // // panic
+    // panic::learning_panic();
+    //
+    // // result
+    // result::learning_result();
+    //
+    // // eliminate duplicated code
+    // eliminate_duplicate_code::learning_eliminate();
+    //
+    // // generics
+    // generics::learning_generics();
+    //
+    // // trait
+    // learning_trait::learning_trait();
+    //
+    // // lifecycle
+    // lifecycle_1::learning_lifecycle();
+    // lifecycle_2::learning_lifecycle();
+    // lifecycle_3::learning_lifecycle();
+    // lifecycle_4::learning_lifecycle();
+    //
+    // // test
+    // learning_test::learning_tests();
+    //
+    // // grep example
+    // example_grep::learning_grep_example();
+    // example_grep::my_grep();
+    //
+    // // closure
+    // closure_1::learning_closure();
+    // closure_2::learning_closure();
+    //
+    // // iterator
+    // iterator_1::learning_iterator();
+    // iterator_2::learning_iterator();
+    // iterator_3::learning_iterator();
+    // iterator_4::learning_iterator();
+    // iterator_5::learning_iterator();
+    // iterator_6::learning_iterator();
+    //
+    // // pub use
+    // learning_pub_use::learning_pub_use();
+    //
+    // // smart pointer
+    // smart_pointer_begin::learning_smart_pointer();
+    // smart_pointer_box::learning_smart_pointer();
+    //
+    // // deref trait
+    // deref_trait_1::learning_deref_trait();
+    // deref_trait_2::learning_deref_trait();
+    //
+    // // drop trait
+    // drop_trait::learning_drop_trait();
+    //
+    // // Rc<T> 引用计数智能指针
+    // rc::learning_rc();
+    //
+    // // RecCell<T>
+    // refcell::learning_ref_cell();
+    //
+    // // memory leak
+    // memory_leak::learning_memory_leak();
+    //
+    // // multi threads
+    // threads_1::learning_threads();
+    // threads_2::learning_threads();
+    // threads_3::learning_threads();
+    // threads_4::learning_threads();
+    //
+    // // oop
+    // oop_1::learning_oop();
+    // oop_2::learning_oop();
+    // oop_3::learning_oop();
+    //
+    // // pattern matching
+    // pattern_matching_1::learning_pattern_matching();
+    // pattern_matching_2::learning_pattern_matching();
+    // pattern_matching_3::learning_pattern_matching();
+    //
+    // // unsafe rust
+    // unsafe_rust::learning_unsafe_rust();
+    //
+    // // advanced trait
+    // advanced_trait::learning_advanced_trait();
+    //
+    // // advanced type
+    // advanced_type::learning_advanced_type();
+    //
+    // // advanced function
+    // advanced_function::learning_advanced_function();
+    //
+    // // marcos
+    // learn_macro::learning_macros();
 }
